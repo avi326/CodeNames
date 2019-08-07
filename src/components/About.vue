42000b7d73292e6304d0ac1b6d537bf0c7c9e435
dhponp <dudupaniri@gmail.com>
2019-08-07 11:49:08 +0300

    Added rules pdf to About page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,17 @@
 <template>
     <div id="about">
-        <h1> about </h1>
+        <h1> Rules: </h1>
+        <pdf src="./static/codenames-rules-he.pdf"></pdf>
     </div>
 </template>
 
 <script>
+import pdf from 'vue-pdf'
+
 export default {
+    components:{
+        pdf
+    },
     name: 'About',
     data () {
         return {
