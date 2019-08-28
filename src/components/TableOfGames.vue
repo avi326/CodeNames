7ca0a2214c3ca773c7c838422ee0f4bd5609f2df
avi326 <avibarazani326@gmail.com>
2019-08-29 02:00:27 +0300

    fix style in table of games

--- src/components/TableOfGames.vue
+++ src/components/TableOfGames.vue
@@ -1,34 +1,50 @@
 <template>
     <div id="TableOfGames">
-        <h1> Table Of Games </h1>
+    <b-container>
+      <b-row>
+        <b-col>
+            <h1 style="text-align: center;" >   לוח משחקים </h1>
+        </b-col>
+      </b-row>
+      <b-row  class="justify-content-md-center">
+        <b-col md="auto">
+          <p v-if="alias">hello {{ alias }} </p>
+          <p v-else> hello guest, please <router-link :to="{ name: 'Signup'}"> signup here </router-link> or <router-link :to="{ name: 'Login'}">
+             login 
+            <i class="fa fa-header" aria-hidden="true"></i> </router-link>  </p>
+        </b-col>
+      </b-row>
+      <b-row  class="justify-content-md-center">
+        <b-col md="auto">
+            <table v-if="alias">
+              <thead>
+                <tr>
+                    <th>player1</th>
+                    <th>player2</th>
+                    <th>count players</th>
+                </tr>
+              </thead>
 
-        <p v-if="alias">hello {{ alias }} </p>
-        <p v-else> hello guest, please <router-link :to="{ name: 'Signup'}"> signup here </router-link> or <router-link :to="{ name: 'Login'}"> login <i class="fa fa-header" aria-hidden="true"></i> </router-link>  </p>
+              <tbody>
+                <tr v-for="game in games" :key="game.id">
+                  <td> {{ game.player1 }} </td>
+                  <td v-if="game.player2==''"> <router-link :to="{ name: 'Game', params: {player_one_alias: game.player1 , player_two_alias: alias , conected_player: alias}}"> Join!  </router-link> </td>
+                  <td v-else> {{ game.player2 }} </td>
+                  <td class="play_now">  {{ game.countPlayers }} </td>
+                  <td> <i class="material-icons delete" @click="deleteGame(game.id)">delete</i> </td>
+                  <td v-if="game.player2 == alias || game.player1 == alias" > <router-link :to="{ name: 'Game', params: {player_one_alias: game.player1 , player_two_alias: alias , conected_player: alias}}"> Resume game </router-link> </td>
 
-      <table v-if="alias">
-        <thead>
-          <tr>
-              <th>Name1</th>
-              <th>Name2</th>
-              <th>num of players now</th>
-          </tr>
-        </thead>
+                </tr>
+                <tr>
+                  <td><NewGame :alias="alias"/></td>
+                </tr>
+              </tbody>
+            </table>
 
-        <tbody>
-          <tr v-for="game in games" :key="game.id">
-            <td> {{ game.player1 }} </td>
-            <td v-if="game.player2==''"> <router-link :to="{ name: 'Game', params: {player_one_alias: game.player1 , player_two_alias: alias , conected_player: alias}}"> Join!  </router-link> </td>
-            <td v-else> {{ game.player2 }} </td>
-            <td class="play_now">  {{ game.countPlayers }} </td>
-            <td> <i class="material-icons delete" @click="deleteGame(game.id)">delete</i> </td>
-            <td v-if="game.player2 == alias || game.player1 == alias" > <router-link :to="{ name: 'Game', params: {player_one_alias: game.player1 , player_two_alias: alias , conected_player: alias}}"> Resume game </router-link> </td>
-
-          </tr>
-          <tr>
-             <td><NewGame :alias="alias"/></td>
-          </tr>
-        </tbody>
-      </table>
+        </b-col>
+      </b-row>
+    </b-container>
+      
 
     </div>
 </template>
@@ -115,7 +131,7 @@ export default {
 <style>
 
 table {
-  max-width: 300px;
+  max-width: 400px;
 }
 
 td .play_now {
